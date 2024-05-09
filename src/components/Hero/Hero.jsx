d9ee5d582a7b7aeb039d60d4ed5ceb7ab0496bec
DricsTheE <matthewli@csu.fullerton.edu>
2024-05-08 18:59:27 -0700

    updated portfolio

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,8 @@
 //Import needed files
 import React from 'react';
 import './Hero.css';
-import python from '../../assets/python.png';
-import react from '../../assets/React.png';
-import svelte from '../../assets/svelte.png';
 import pfp from '../../assets/Portrait.jpg'
-import javascript from '../../assets/javascript.png';
+
 
 const Hero = () => {
     return ( 
@@ -13,36 +10,18 @@ const Hero = () => {
             <div className="hero-content">
                 {/* Displays main text and sub text */}
                 <h2>Matthew Li</h2>
-                <p>
-                    Welcome to my Portfolio Page!
-                </p>
+                <h4> Welcome to my Portfolio Page! </h4> <br />
+                <p> I am currently a senior in CSUF studying in the Department of Computer Science and Engineering program, working towards a bachelors degree.</p>
             </div>
 
             {/* Div for images and portrait */}
             <div className="hero-img">
                 {/* Create one showcased tech image to the side */}
                 <div>
-                    {/* Creates the display border for images */}
-                    <div className="tech-icon">
-                        {/* Displays image */}
-                        <img className='image' src={react} alt="react" />
-                    </div>
                     {/* Displays portrait */}
                     <img className='pfp-img' src={pfp} alt="" />
                 </div>
 
-                {/* List the rest of the icons in a row */}
-                <div>
-                    <div className="tech-icon">
-                        <img  className='image' src={python} alt="" />
-                    </div>
-                    <div className="tech-icon">
-                        <img className='image'  src={svelte} alt="" />
-                    </div>
-                    <div className="tech-icon">
-                        <img  className='image' src={javascript} alt="" />
-                    </div>
-                </div>
             </div>
         </section>
     
